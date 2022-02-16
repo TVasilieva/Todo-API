83d37264ef9e7cd504989af992576e1418dc8a1d
Tonya Vasilieva <tonya1997vasilieva@gmail.com>
2022-02-16 14:06:33 +0300

    feat: added sorted array

--- src/components/ListOfTodos/index.tsx
+++ src/components/ListOfTodos/index.tsx
@@ -1,18 +1,21 @@
 import React, { FC } from "react";
 import "./style.css";
-import { todos } from "../../todos";
 import TodoItem from "../TodoItem";
 
 import ComponentProps from "./types";
 
-const ListOfTodos: FC<ComponentProps> = () => {
+const ListOfTodos: FC<ComponentProps> = ({ todos }) => {
   const todoItems = todos.map((el: any) => {
     return <TodoItem key={el.id} data={el.name} />;
   });
 
   return (
     <>
-      <div className="listOfTodos">{todoItems}</div>
+      {todos.length ? (
+        <div className="listOfTodos">{todoItems}</div>
+      ) : (
+        <div className="listOfEmptyTodos">Your list of todos is empty</div>
+      )}
     </>
   );
 };
--- src/pages/AppPage/index.tsx
+++ src/pages/AppPage/index.tsx
@@ -3,18 +3,19 @@ import InputForAdding from "../../components/InputForAdding";
 import ListOfTodos from "../../components/ListOfTodos";
 import ShownItems from "../../components/ShownItems";
 
-import { todos } from "../../todos";
+import { Todos, todos } from "../../todos";
 import ComponentProps from "./types";
 import "./style.css";
 
 const AppPage: FC<ComponentProps> = () => {
   const [completed, setCompleted] = useState<boolean>(false);
+  const [sortedTodos, setSortedTodos] = useState<Todos[]>([]);
 
   return (
     <div className="user-page">
       <h1>todo</h1>
       <InputForAdding />
-      <ListOfTodos todos={todos} />
+      <ListOfTodos todos={sortedTodos} />
       <ShownItems />
     </div>
   );
