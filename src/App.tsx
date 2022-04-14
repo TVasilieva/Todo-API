ae584c1f2e702dce99fc473556bc12aceff55af8
Antonina Vasilieva <tonya_verkooijen@mail.ru>
2022-04-14 20:42:13 +0300

    fix: fixed deployed version

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.scss";
 
